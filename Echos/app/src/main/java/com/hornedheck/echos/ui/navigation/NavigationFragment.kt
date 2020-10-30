5384279ae683be003876e17a0fcb316645d110d5
Vitaly Avseitsev <vitaly.avseitsev@gmail.com>
2020-10-30 12:39:27 +0300

    navigation menu

--- Echos/app/src/main/java/com/hornedheck/echos/ui/navigation/NavigationFragment.kt
+++ Echos/app/src/main/java/com/hornedheck/echos/ui/navigation/NavigationFragment.kt
@@ -7,10 +7,37 @@ import androidx.appcompat.app.AppCompatActivity
 import com.hornedheck.echos.R
 import com.hornedheck.echos.base.BaseFragment
 import com.hornedheck.echos.di.LocalFlowComponent
+import com.hornedheck.echos.navigation.EchosNavigator
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.fragment_navigation.*
+import moxy.presenter.InjectPresenter
+import moxy.presenter.ProvidePresenter
+import ru.terrakok.cicerone.NavigatorHolder
+import ru.terrakok.cicerone.Router
+import javax.inject.Inject
 
-class NavigationFragment : BaseFragment(R.layout.fragment_navigation) {
+class NavigationFragment : BaseFragment(R.layout.fragment_navigation), NavigationView {
+
+    @Inject
+    @InjectPresenter
+    lateinit var presenter: NavigationPresenter
+
+    @ProvidePresenter
+    fun provide() = presenter
+
+    @Inject
+    lateinit var router: Router
+
+    @Inject
+    lateinit var navHolder: NavigatorHolder
+
+    private val navigator by lazy {
+        EchosNavigator(
+            requireActivity(),
+            R.id.flFragmentContainer,
+            childFragmentManager
+        )
+    }
 
     override val localFlowComponent: LocalFlowComponent
         get() = appComponent.getLocalFlowComponent()
@@ -19,19 +46,36 @@ class NavigationFragment : BaseFragment(R.layout.fragment_navigation) {
         localFlowComponent.inject(this)
     }
 
+    override fun onResume() {
+        super.onResume()
+        navHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navHolder.removeNavigator()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(requireActivity() as AppCompatActivity) {
             setSupportActionBar(toolbar)
         }
         val toggle = ActionBarDrawerToggle(requireActivity(),
-            drawer_layout,
+            drawerLayout,
             toolbar,
             R.string.navigation_drawer_open,
             R.string.navigation_drawer_close)
 
         toggle.isDrawerIndicatorEnabled = true
         toggle.syncState()
+        navView.setNavigationItemSelectedListener { item ->
+            presenter.navigate(item.itemId).also {
+                if (it) drawerLayout.closeDrawers()
+            }
+        }
+        navView.setCheckedItem(R.id.menu_contacts)
+        presenter.navigate(R.id.menu_contacts)
     }
 
 }
--- Echos/app/src/main/java/com/hornedheck/echos/ui/navigation/NavigationPresenter.kt
+++ Echos/app/src/main/java/com/hornedheck/echos/ui/navigation/NavigationPresenter.kt
@@ -1,12 +1,37 @@
 package com.hornedheck.echos.ui.navigation
 
+import com.hornedheck.echos.R
 import com.hornedheck.echos.base.BasePresenter
+import com.hornedheck.echos.navigation.ContactsScreen
 import ru.terrakok.cicerone.Router
 import javax.inject.Inject
 
 class NavigationPresenter @Inject constructor(
     private val router: Router,
 ) : BasePresenter<NavigationView>() {
+
+    private var lastNav = 0
+
+    fun navigate(to: Int): Boolean {
+        if (to == lastNav) return true
+        return when (to) {
+            R.id.menu_contacts -> {
+                router.replaceScreen(ContactsScreen())
+                lastNav = to
+                true
+            }
+            R.id.menu_settings -> {
+                viewState.showError("Not implemented", "Not implemented")
+                false
+            }
+            R.id.menu_logout -> {
+                viewState.showError("Not implemented", "Not implemented")
+                false
+            }
+            else -> false
+        }
+
+    }
 
 
 }
